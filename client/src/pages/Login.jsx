2365f52d7dffc3de91e90dda6cc2cd8024260dec
deveshyadav28 <deveshyadav.official24@gmail.com>
2026-09-22 16:23:31 +0530

    create a home , signup and login pages

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+function Login() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+
+      <h1 className="text-3xl font-bold">
+        Login
+      </h1>
+
+    </div>
+  );
+}
+
+export default Login;
---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div className="min-h-screen bg-slate-50">
+
+      <h1 className="text-4xl font-bold text-center pt-20">
+        CivicFix
+      </h1>
+
+      <p className="text-center mt-4 text-slate-600">
+        Smart Civic Complaint & Resolution System
+      </p>
+
+    </div>
+  );
+}
+
+export default Home;
