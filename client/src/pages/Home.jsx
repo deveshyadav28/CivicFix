a1ce5c113547e6d98913260b12ae75bee937371f
deveshyadav28 <deveshyadav.official24@gmail.com>
2026-09-22 16:27:03 +0530

    update pages

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
-const Home = () => {
+function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="min-h-screen bg-slate-50">
+
+      <h1 className="text-4xl font-bold text-center pt-20">
+        CivicFix
+      </h1>
+
+      <p className="text-center mt-4 text-slate-600">
+        Smart Civic Complaint & Resolution System
+      </p>
+
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,17 +1,13 @@
-function Home() {
+function Signup() {
   return (
-    <div className="min-h-screen bg-slate-50">
+    <div className="min-h-screen flex items-center justify-center">
 
-      <h1 className="text-4xl font-bold text-center pt-20">
-        CivicFix
+      <h1 className="text-3xl font-bold">
+        Signup
       </h1>
-
-      <p className="text-center mt-4 text-slate-600">
-        Smart Civic Complaint & Resolution System
-      </p>
 
     </div>
   );
 }
 
-export default Home;
+export default Signup;
